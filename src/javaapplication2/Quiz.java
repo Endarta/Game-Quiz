0dd599864bc3a670ad5cb0333ca66126d0483f91
Endarta <endartasyafii99@gmail.com>
2018-06-05 06:13:02 +0900

    commit pertama

---
+++ src/javaapplication2/Quiz.java
@@ -0,0 +1,19 @@
+package javaapplication2;
+
+public class Quiz {
+    private String soal;
+    private String jawab;
+    public Quiz(String soal, String jawab) {
+        this.soal = soal;
+        this.jawab = jawab;
+    }
+    public boolean cocokanJawaban(String jawab) {
+        return this.jawab.equalsIgnoreCase(jawab);
+    }
+    public String getJawab() {
+        return jawab;
+    }
+    public String getSoal() {
+        return soal;
+    }
+}
